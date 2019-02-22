0860797b60a9769286e9d491406a7a9587311193
vicidel <vicdelafont@gmail.com>
2019-02-22 16:42:13 +0100

    optimization on trilateration

--- matlab_sim/drone_sim2.m
+++ matlab_sim/drone_sim2.m
@@ -7,12 +7,12 @@
 clear all; close all;
 
 % define node coordinates xyz (z altitude)
-arena_size = 50;
+arena_size = 100;
 node_position = [rand*2*arena_size-arena_size, rand*2*arena_size-arena_size, 0];
 drone_position = [0, 0, 10];
-measure_position1 = [rand*100, rand*100, 10];    % x > 0, y > 0
-measure_position2 = [-rand*100, rand*100, 10];    % x < 0, y > 0
-measure_position3 = [rand*200-100, rand*100-100, 10];    % y < 0
+measure_position1 = [rand*arena_size, rand*arena_size, 10];    % x > 0, y > 0
+measure_position2 = [-rand*arena_size, rand*arena_size, 10];    % x < 0, y > 0
+measure_position3 = [rand*2*arena_size-arena_size, rand*arena_size-arena_size, 10];    % y < 0
 distance = 9999;
 
 % init
@@ -27,25 +27,31 @@ load('polynom_ESP_to_dist.mat', 'fitresult_ESPd');
 p_distance_from_ESP = fitresult_ESPd;
 
 % parameters
-noise_level = 4; % +- 2dB
+noise_level = 3; % +- 2dB
 time_limit = 1000;
 pause_time = 0; % seconds
+end_condition_distance = 10;
+algo_loop = 0;
+size_around_est = 40;
+plot_bool = false;
+
+% set first figure
+if plot_bool 
+    figure(1);
+end
 
 % localization
 while time < time_limit
     
-    % plot positions
-    plot3(node_position(1), node_position(2), node_position(3), 'ko', 'MarkerSize', 10); grid on; hold on;
-    plot3(drone_position(1), drone_position(2), drone_position(3), 'co', 'MarkerSize', 10);
-    plot3(measure_position1(1), measure_position1(2), measure_position1(3), 'ro', 'MarkerSize', 10);
-    plot3(measure_position2(1), measure_position2(2), measure_position2(3), 'go', 'MarkerSize', 10);
-    plot3(measure_position3(1), measure_position3(2), measure_position3(3), 'bo', 'MarkerSize', 10);
-%     xlabel('x position [m]')
-%     ylabel('y position [m]')
-%     zlabel('z position [m]')
-%     title('Drone movement');
-%     legend('Node position', 'Drone position', '1st measure', '2nd measure', '3rd measure');
-    axis square; view(0, 90);
+    if plot_bool
+        % plot positions
+        plot3(node_position(1), node_position(2), node_position(3), 'ko', 'MarkerSize', 10); grid on; hold on;
+        plot3(drone_position(1), drone_position(2), drone_position(3), 'co', 'MarkerSize', 10);
+        plot3(measure_position1(1), measure_position1(2), measure_position1(3), 'ro', 'MarkerSize', 10);
+        plot3(measure_position2(1), measure_position2(2), measure_position2(3), 'go', 'MarkerSize', 10);
+        plot3(measure_position3(1), measure_position3(2), measure_position3(3), 'bo', 'MarkerSize', 10);
+        axis square; view(0, 90);
+    end
     
     switch state
         case 0 % starting point
@@ -101,16 +107,68 @@ while time < time_limit
             [x, y] = get_position(measure_position1(1), measure_position1(2), distance_from_ESP(ESP(1), p_distance_from_ESP), ...
                                   measure_position2(1), measure_position2(2), distance_from_ESP(ESP(2), p_distance_from_ESP), ...
                                   measure_position3(1), measure_position3(2), distance_from_ESP(ESP(3), p_distance_from_ESP));
-            estimated_position = [x, y];
-            if isnan(estimated_position)    
-                fprintf('Could not find intersection, redoing now');
+            estimated_position = [x, y, 10];
+            if isnan(estimated_position(1)) || isnan(estimated_position(2))
                 state = 0;
-                measure_position1 = [rand*100, rand*100, 10];    % x > 0, y > 0
-                measure_position2 = [-rand*100, rand*100, 10];    % x < 0, y > 0
-                measure_position3 = [rand*200-100, rand*100-100, 10];    % y < 0
+                if algo_loop == 0
+                    fprintf('Could not find intersection, new random positions\n');
+                    measure_position1 = [rand*100, rand*100, 10];    % x > 0, y > 0
+                    measure_position2 = [-rand*100, rand*100, 10];    % x < 0, y > 0
+                    measure_position3 = [rand*200-100, rand*100-100, 10];    % y < 0
+                else
+                    size_around_est = 60;
+                    fprintf('Could not find intersection, larger spacing (%d m)\n', size_around_est);
+                    measure_position1 = old_estimated_position + [0, -size_around_est, 0];   % south
+                    measure_position2 = old_estimated_position + [size_around_est*cos(pi/6), size_around_est*sin(pi/6), 0];   % north east	
+                    measure_position3 = old_estimated_position + [-size_around_est*cos(pi/6), size_around_est*sin(pi/6), 0];   % north west
+                end
                 close all;
             else
-                break;
+                fprintf('Found best intersection at x=%f and y=%f\n', x, y);
+                
+                if plot_bool
+                    % plot positions and circles
+                    figure();
+                    plot3(node_position(1), node_position(2), node_position(3), 'ko', 'MarkerSize', 10); grid on; hold on;
+                    plot3(measure_position1(1), measure_position1(2), measure_position1(3), 'ro', 'MarkerSize', 10);
+                    plot3(measure_position2(1), measure_position2(2), measure_position2(3), 'go', 'MarkerSize', 10);
+                    plot3(measure_position3(1), measure_position3(2), measure_position3(3), 'bo', 'MarkerSize', 10);
+                    plot3(estimated_position(1), estimated_position(2), estimated_position(3), 'mx', 'MarkerSize', 10);
+                    plot_circle(measure_position1(1), measure_position1(2), distance_from_ESP(ESP(1), p_distance_from_ESP), 'r');
+                    plot_circle(measure_position2(1), measure_position2(2), distance_from_ESP(ESP(2), p_distance_from_ESP), 'g');
+                    plot_circle(measure_position3(1), measure_position3(2), distance_from_ESP(ESP(3), p_distance_from_ESP), 'b');
+                    xlabel('x position [m]')
+                    ylabel('y position [m]')
+                    zlabel('z position [m]')
+                    title('Node localization algorithm');
+                    legend('Node position', '1st measure', '2nd measure', '3rd measure', 'Estimated position');
+                    view(0, 90); axis square; 
+                end
+
+                % print in terminal
+                fprintf('Estimated position of node: x=%.2f, y=%.2f\n', estimated_position(1), estimated_position(2));
+                fprintf('Real position of node: x=%.2f, y=%.2f\n', node_position(1), node_position(2));
+                fprintf('Error: dx=%.2f, dy=%.2f, norm=%.2f\n', abs(estimated_position(1) - node_position(1)), abs(estimated_position(2) - node_position(2)), norm([abs(estimated_position(1) - node_position(1)), abs(estimated_position(2) - node_position(2))])); 
+                
+                % end condition
+                if algo_loop == 1
+                    fprintf('End of algorithm\n');
+                    break;
+                end
+                
+                % smaller size
+                measure_position1 = estimated_position + [0, -size_around_est, 0];   % south
+                measure_position2 = estimated_position + [size_around_est*cos(pi/6), size_around_est*sin(pi/6), 0];   % north east	
+                measure_position3 = estimated_position + [-size_around_est*cos(pi/6), size_around_est*sin(pi/6), 0];   % north west
+                
+                % return to beginning
+                fprintf('Restarting algorithm around found position\n\n');
+                if plot_bool
+                    figure();
+                end
+                state = 0;
+                algo_loop = 1;
+                old_estimated_position = estimated_position;
             end
             
     end
@@ -122,28 +180,6 @@ while time < time_limit
     pause(pause_time);
 end
 
-
-% plot positions and circles
-figure();
-plot3(node_position(1), node_position(2), node_position(3), 'ko', 'MarkerSize', 10); grid on; hold on;
-plot3(measure_position1(1), measure_position1(2), measure_position1(3), 'ro', 'MarkerSize', 10);
-plot3(measure_position2(1), measure_position2(2), measure_position2(3), 'go', 'MarkerSize', 10);
-plot3(measure_position3(1), measure_position3(2), measure_position3(3), 'bo', 'MarkerSize', 10);
-plot3(estimated_position(1), estimated_position(2), 10, 'mx', 'MarkerSize', 10);
-plot_circle(measure_position1(1), measure_position1(2), distance_from_ESP(ESP(1), p_distance_from_ESP), 'r');
-plot_circle(measure_position2(1), measure_position2(2), distance_from_ESP(ESP(2), p_distance_from_ESP), 'g');
-plot_circle(measure_position3(1), measure_position3(2), distance_from_ESP(ESP(3), p_distance_from_ESP), 'b');
-xlabel('x position [m]')
-ylabel('y position [m]')
-zlabel('z position [m]')
-title('Node localization algorithm');
-legend('Node position', '1st measure', '2nd measure', '3rd measure', 'Estimated position');
-view(0, 90); axis square; 
-
-% print
-fprintf('\nEstimated position of node: x=%.2f, y=%.2f\n', estimated_position(1), estimated_position(2));
-fprintf('Real position of node: x=%.2f, y=%.2f\n', node_position(1), node_position(2));
-fprintf('Error: dx=%.2f, dy=%.2f, norm=%.2f\n', abs(estimated_position(1) - node_position(1)), abs(estimated_position(2) - node_position(2)), norm([abs(drone_position(1) - node_position(1)), abs(drone_position(2) - node_position(2))])); 
 
 
 % plots a circle in x, y, radius r in defined color 

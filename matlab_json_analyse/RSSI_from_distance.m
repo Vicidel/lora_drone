8c86a17fcddf22850c265a855d6e2370f0826765
vicidel <vicdelafont@gmail.com>
2019-02-22 13:59:36 +0100

    to avoid re-growth of parabola after certain distance

--- matlab_json_analyse/RSSI_from_distance.m
+++ matlab_json_analyse/RSSI_from_distance.m
@@ -2,6 +2,10 @@ function [RSSI, confidence_interval_95] = RSSI_from_distance(distance, p)
 
 ci = confint(p);
 
+if distance > 160       % to remove re-growth of parabola after certain distance
+    distance = 160;
+end
+
 % get results
 RSSI = distance^2 * p.p1 + distance * p.p2 + p.p3;
 confidence_interval_95(:) = distance^2 * ci(:,1) + distance * ci(:,2) + ci(:,3);

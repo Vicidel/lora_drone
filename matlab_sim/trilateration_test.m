d171d7d392b8de0b95643d2f9db549dc1df9ceb4
vicidel <vicdelafont@gmail.com>
2019-02-26 16:13:10 +0100

    continued on pylocus, still not working

--- matlab_sim/trilateration_test.m
+++ matlab_sim/trilateration_test.m
@@ -29,7 +29,7 @@ S = [measured_distance_1 measured_distance_2 measured_distance_3];
 W = ones(size(S));
 
 % trilateration
-[x, y] = get_position(P, S, W);
+[x, y] = pylocus_get_position(P, S, W);
 
 % plot 
 figure();
@@ -38,6 +38,7 @@ plot_tri(network_position, 'co');
 plot_tri(drone_position_1, 'ro');
 plot_tri(drone_position_2, 'go');
 plot_tri(drone_position_3, 'bo');
+plot3(x, y, 0, 'mx');
 plot_circle(drone_position_1(1), drone_position_1(2), measured_distance_1, 'r');
 plot_circle(drone_position_2(1), drone_position_2(2), measured_distance_2, 'g');
 plot_circle(drone_position_3(1), drone_position_3(2), measured_distance_3, 'b');
@@ -48,23 +49,23 @@ zlabel('z position [m]')
 title('Localization with three drones - First measure');
 legend('Real position', 'Network position', '1st drone', '2nd drone', '3rd drone', 'Estimated position');
 
-
+a = 1;
 
 
 
 
 % obtain the position of two intersection of three circles (in 3D)
 % P is of size d.N with dimension d and N points, W weights (usually ones)
-function [x, y] = get_position(P, S, W)
+function [x, y] = pylocus_get_position(P, S, W)
     
     [d, N] = size(P(1:2, :));
-
+    
     % for each point set A and b matrices
     A = []; b = [];
     for i=1: N
         x = P(1,i); y = P(2,i);
         s = S(i);
-        A = [A ; 1 -2*x  -2*y]; 
+        A = [A ; -2*x  -2*y  1]; 
         b = [b ; s^2-x^2-y^2];
     end
     ATA = A.' * A;
@@ -75,10 +76,11 @@ function [x, y] = get_position(P, S, W)
     f = [zeros(1, d), -0.5]';
     
     % eigenvalues
-    eigen = eig(D, ATA);
+    eigen = sort(eig(D, ATA));
     if eigen(end) > 1e-10
         lower_bound = -1/eigen(end);
     else
+        fprintf('Warning: biggest eigenvalue is zero\n');
         lower_bound = -1e-5;
     end
     
@@ -87,17 +89,41 @@ function [x, y] = get_position(P, S, W)
     lambda_opt = 0;
     
     % find optimal lambda
-    'TODO'
+    temp1 = pylocus_phi(lower_bound, ATA, D, A, b, f);
+    temp2 = pylocus_phi(inf, ATA, D, A, b, f);
+    if temp1 > 0 && temp2 < 0
+        min_func = @(lambda) ((ATA + lambda*D)\(A*b - lambda*f))' * D * ((ATA + lambda*D)\(A*b - lambda*f)) + 2 * f' * ((ATA + lambda*D)\(A*b - lambda*f));
+        %lambda_opt = fminsearch(min_func, lambda_opt);
+        lambda_opt = fminbnd(min_func, lower_bound, inf);
+    else
+        fprintf('Lambda optimal set as 0...\n');
+    end
     
-    % solve ?
-    lhs = ATA + lambda_opt*D;
+    % get solution
     rhs = A*b - lambda_opt*f;
-    y = lhs\rhs;
+    lhs = ATA + lambda_opt*D;
+    yhat = lhs\rhs;
     
-    
-    x = 0;
-    y = 0;
+    x = yhat(1);
+    y = yhat(2);
 
+end
+
+% for pylocus
+function yhat = pylocus_yhat(lambda, ATA, D, A, b, f)
+    
+    rhs = A*b - lambda*f;
+    lhs = ATA + lambda*D;
+    
+    yhat = lhs\rhs;
+end
+
+% for pylocus
+function phi = pylocus_phi(lambda, ATA, D, A, b, f)
+
+    yhat = pylocus_yhat(lambda, ATA, D, A, b, f);
+    phi = yhat' * D * yhat + 2 * f' * yhat;
+    
 end
 
 % plot a vector of 3x1 in defined color

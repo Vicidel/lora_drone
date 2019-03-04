d0f3bfb7536454461075c6e52040b298dc277c76
vicidel <vicdelafont@gmail.com>
2019-03-04 14:59:08 +0100

    eighth wavelength, not half

--- matlab_signal_noise/signal_attenuation_angle.m
+++ matlab_signal_noise/signal_attenuation_angle.m
@@ -7,9 +7,9 @@ function coefficient = signal_attenuation_angle(theta_deg)
     c = 299792458;
     lambda = c/freq;
     omega = 2*pi*freq;
-    L_half = lambda/2;
-    beta = 2*pi/L_half;
-    E_theta_half = 1i * exp(1i * omega * (1)) * (cos(cos(theta) * beta * L_half / 2) - cos(beta * L_half / 2)) / (2 * sin(theta));
+    L = lambda/8;
+    beta = 2*pi/L;
+    E_theta_half = 1i * exp(1i * omega * (1)) * (cos(cos(theta) * beta * L / 2) - cos(beta * L / 2)) / (2 * sin(theta));
     
     coefficient = norm(E_theta_half);
     

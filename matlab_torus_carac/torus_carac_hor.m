06873008b2082d8b46a600b6d56857c98836afd4
vicidel <vicdelafont@gmail.com>
2019-03-06 16:41:48 +0100

    torus analysis file

---
+++ matlab_torus_carac/torus_carac_hor.m
@@ -0,0 +1,27 @@
+%% DECODING SECTION
+clear all; close all;
+
+% opens the JSON file decodes it
+fname = 'json_backup/20190306pm_torus_horizontal.json';
+[time, angles, SF, RSSI, ESP, SNR] = decode_json(fname);
+
+% angles
+angles_list = [0, 45, 90, 135, 180, 225, 270, 315];
+
+% number of messages received
+nb_0 = sum(angles==1);
+nb_45 = sum(angles==2);
+nb_90 = sum(angles==3);
+nb_135 = sum(angles==4);
+nb_180 = sum(angles==5);
+nb_225 = sum(angles==6);
+nb_270 = sum(angles==7);
+nb_315 = sum(angles==8);
+
+
+%% PLOTTING SECTION
+
+% plot ESP against angles
+figure();
+plot(time, ESP, 'r'); grid on; hold on;
+plot(time, RSSI, 'b');

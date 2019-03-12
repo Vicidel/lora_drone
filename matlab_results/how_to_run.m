a6e8a90a56dcc6abcdd6ca4e80ef3e34857e0234
vicidel <vicdelafont@gmail.com>
2019-03-12 16:39:28 +0100

    added printf

--- matlab_results/how_to_run.m
+++ matlab_results/how_to_run.m
@@ -5,6 +5,7 @@
 % clear and close
 clear all;
 close all;
+clc;
 
 % set the position of drone (gateway) and node 
 drone_position = [100, 100, 10];
@@ -20,27 +21,33 @@ node_antenna = 0;           % 0 for towards sky
 % get the distance from drone to node
 distances = drone_position - node_position;
 distance_norm = norm(distances);
+fprintf('The distance between the two devices is %.2f meters \n', distance_norm');
 
 % get the angles
 % ((for now we consider that the two are vertical))
 distance_ground = sqrt(distances(1)^2+distances(2)^2);
 angle_rad = atan(distances(3)/distance_ground);
 angle_deg = angle_rad*180/pi;
+fprintf('The angle between them is %.2f degrees\n', angle_deg);
 
 
 %% GET FREE-SPACE PATH LOSS
 % get the true signal from the distance
 ESP_true = func_distance_to_signal(distance_norm, 'esp');
+fprintf('From the distance, we obtain a noise-free free-space path loss of %.2f dBm\n', ESP_true);
 
 % add 2.5 dB noise
 ESP_noisy = ESP_true + normrnd(0, 2.5);
+fprintf('With added normal noise of std=2.5dB: %.2f dBm\n', ESP_noisy);
 
 
 %% GET ANGLE ATTENUATIONS
 % get attenuation in db
 attenuation_db = func_attenuation_angle(angle_deg);
+fprintf('The attenuation due to the angle between them is %.2f dB\n', attenuation_db);
 
 
 %% SUM THE TWO COMPONENTS
 % sum it to noisy signal
 ESP_final = ESP_noisy + attenuation_db;
+fprintf('The final simulated ESP is %.2f dBm\n', ESP_final);

747551ec8836a95f393936a181d83bf631e4b88c
vicidel <vicdelafont@gmail.com>
2019-03-26 08:58:24 +0100

    new gradient plot

--- matlab_json_analyse/signal_2d_plot.m
+++ matlab_json_analyse/signal_2d_plot.m
@@ -1,32 +1,34 @@
 clear all; close all;
 
 % parameters
-limit = 50;
-node_position = [rand*limit-limit/2, rand*limit-limit/2 0];
-x_values = -limit:1:limit;
-y_values = -limit:1:limit;
+limit = 70;
+node_position = [0, 0, 0];
+x_values = -50:1:50;
+y_values = -50:1:50;
 altitude = 10;
 
 % result
-matrix = zeros(length(x_values), length(y_values));
+distance = zeros(length(x_values), length(y_values));
+signal = zeros(length(x_values), length(y_values));
 
 % fills result matrix
 for i=1: length(x_values)
     for j=1: length(y_values)
         x = x_values(i);
         y = y_values(j);
-        distance = norm([x-node_position(1), y-node_position(2), altitude]);
-        matrix(i, j) = func_distance_to_signal(distance, 'esp');
+        distance(i,j) = norm([x-node_position(1), y-node_position(2), altitude]);
+        signal(i,j) = func_distance_to_signal(distance(i,j), 'esp');
     end
 end
 
 % plot
 figure();
-s = surf(x_values, y_values, matrix);
+s = surf(y_values, x_values, signal);
 s.EdgeColor = 'none';
 xlabel('x [m]');
 ylabel('y [m]');
 zlabel('ESP [dBm]');
 title('ESP gradient');
+axis([-50 50 -50 50]);
 view(0, 90);
 colorbar;

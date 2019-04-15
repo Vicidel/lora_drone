81934717b2d9497987a69d4e84c79a52f93a71fa
Victor Delafontaine <vicdelafont@gmail.com>
2019-04-15 15:20:26 +0200

    progress on app, no more database for drone

--- server_app/app_backup_20190415.py
+++ server_app/app_backup_20190415.py
@@ -11,12 +11,6 @@ from itertools import chain
 
 
 
-
-
-#########################################################################################
-####################################  APP PARAMETERS  ###################################
-#########################################################################################
-
 # bootstrap the app
 app = Flask(__name__)
 
@@ -33,16 +27,8 @@ app.config['MONGODB_SETTINGS'] = {'host': mongo_uri, 'db':'testing', 'alias':'de
 # bootstrap our app
 db = MongoEngine(app)
 
-# set the port dynamically with a default of 3000 for local development
-port = int(os.getenv('PORT', '3000'))
 
-
-
-#########################################################################################
-#################################  CLASSES AND DATASETS  ################################
-#########################################################################################
-
-# class for LoRa messages
+# create the two classes for LoRa and drone messages
 class LoRa_datapoint(db.Document):
 	devEUI       = db.StringField(required=True)
 	deviceType   = db.StringField()
@@ -58,32 +44,24 @@ class LoRa_datapoint(db.Document):
 	tx_power     = db.FloatField()
 	real_dist 	 = db.IntField()
 
-# class and dataset for drone messages 
-drone_dataset = []
-class drone_datapoint:
-	pos_x 		 = 666
-	pos_y 		 = 666
-	pos_z 		 = 666
-	timestamp 	 = 666
-	time 		 = ''
-	payload 	 = ''
-	state  		 = 666
+class drone_datapoint(db.Document):
+	pos_x 		 = db.FloatField()
+	pos_y 		 = db.FloatField()
+	pos_z 		 = db.FloatField()
+	timestamp    = db.DateTimeField()
+	time 		 = db.StringField()
+	payload      = db.StringField()
 
-# class for trilateration data	 
-tri_dataset = []
-class tri_datapoint:
-	pos_x 		= 666
-	pos_y 		= 666
-	pos_z 		= 666
-	rssi 		= 666
-	esp 		= 666
-	distance 	= 666
+# create the class for server memory
+class server_memory(db.Document):
+	state 		 = db.IntField()
+	timestamp    = db.DateTimeField()
+	time 		 = db.StringField()
 
 
 
-#########################################################################################
-###################################  LORA PARAMETERS  ###################################
-#########################################################################################
+# set the port dynamically with a default of 3000 for local development
+port = int(os.getenv('PORT', '3000'))
 
 # devices
 gateway_printer = '004A0DB4'
@@ -95,12 +73,6 @@ device_eui      = '78AF580300000493'
 TIME_FORMAT       = "%Y-%m-%dT%H:%M:%S.%f+02:00"
 TIME_FORMAT_QUERY = "%Y-%m-%dT%H:%M:%S"
 
-
-
-#########################################################################################
-##################################  DRONE PARAMETERS  ###################################
-#########################################################################################
-
 # waypoint parameters
 flying_altitude   = 10
 takeoff_altitude  = 2
@@ -108,11 +80,8 @@ network_x 		  = 0
 network_y 		  = 100
 network_z 		  = 0
 circle_radius     = 50
-hover_time 		  = 6
+hover_time 		  = 2
 
-# state and return string
-current_state 	  = 666
-return_string 	  = 'ERROR, return string not set'
 
 
 
@@ -120,56 +89,13 @@ return_string 	  = 'ERROR, return string not set'
 ###################################  TRILATERATION  #####################################
 #########################################################################################
 
-def function_signal_to_distance(esp, rssi):
-
-	# exponential coefficents
-	a_ESP  = 0.2011
-	b_ESP  = -0.0556
-	a_RSSI = 0.0273
-	b_RSSI = -0.0777
-
-	# return the distance from ESP or RSSI
-	return a_ESP * math.exp(b_ESP * esp)
-	# return a_RSSI * math.exp(b_RSSI * rssi)
-
-
-
-def trilateration(drone_dataset):
-
-	# matches LoRa and drone datasets
-	for i in range(0, len(drone_dataset)):
-
-		# ignore unrelevant states
-		if drone_dataset[i].state not in {1,2,3}:
-			continue
-
-		# find matching LoRa points on interval
-		timestamp = (drone_dataset[i].timestamp - dt.datetime(1970,1,1)).total_seconds()
-		start = dt.datetime.fromtimestamp(timestamp-2)
-		end = dt.datetime.fromtimestamp(timestamp+2)
-		lora_data_in_interval = LoRa_datapoint.objects(timestamp__lt=end,timestamp__gt=start).first()
-
-		# create tri dataset
-		datapoint = tri_datapoint()
-		datapoint.pos_x = drone_dataset[i].pos_x
-		datapoint.pos_y = drone_dataset[i].pos_y
-		datapoint.pos_z = drone_dataset[i].pos_z
-		datapoint.esp   = lora_data_in_interval.gateway_esp
-		datapoint.rssi  = lora_data_in_interval.gateway_rssi
-
-		# get distance estimate
-		datapoint.distance = function_signal_to_distance(datapoint.esp, datapoint.rssi)
-
-		# print stuff
-		print("data: x{} y{} esp{} rssi {} dist {}".format(datapoint.pos_x, datapoint.pos_y, datapoint.esp, datapoint.rssi, datapoint.distance))
-
-	# call Pylocus
-	print("TODO: Pylocus")
+# trilateration functions
+def trilateration():
 
 	# dummy position for testing
 	pos_x = -100
 	pos_y = 0
-	pos_z = flying_altitude
+	pos_z = takeoff_altitude
 
 	return pos_x, pos_y, pos_z
 
@@ -204,8 +130,8 @@ def lora_receive():
 		return 'Can only receive JSON file'
 
 	# display in log the JSON received
-	#print("JSON received:")
-	#print(j)
+	print("JSON received:")
+	print(j)
 
 	# parse communication parameters
 	r_deveui    = j['DevEUI_uplink']['DevEUI']
@@ -279,8 +205,6 @@ def lora_receive():
 # receive GPS coordinates from offb_node script
 @app.route('/drone/receive_message', methods=['POST'])
 def drone_receive():
-
-	###################  DECODE PAYLOAD  ######################
 	print("!!! Data received from drone !!!")
 	
 	# test nature of message: if not JSON we don't want it
@@ -295,7 +219,7 @@ def drone_receive():
 	print("JSON received:")
 	print(j)
 
-	# parse received data
+	# parse communication parameters
 	r_pos_x     = j['pos_x']
 	r_pos_y     = j['pos_y']
 	r_pos_z     = j['pos_z']
@@ -307,90 +231,73 @@ def drone_receive():
 	r_timestamp = dt.datetime.utcfromtimestamp(float(r_ts_temp))
 	r_time 		= str(r_time) 
 
+	# create new datapoint with parsed data
+	datapoint = drone_datapoint(pos_x=r_pos_x, pos_y=r_pos_y, pos_z=r_pos_z, time=r_time, timestamp=r_timestamp, payload=r_payload)
 
-	###################  SWITCH STATE  ######################
-	# get global variables from previous
-	global drone_dataset
-	global current_state
+	# save it to database
+	datapoint.save()
+	print('SUCESS: new drone datapoint saved to database')
 
-	# switch to OFFBOARD mode done
-	if r_payload=='drone_offboard':
-		return_string = "Congrats on offboard mode"
-
-	# drone was just armed
+	# process payload
 	if r_payload=='drone_armed':
-
 		# set takeoff position as 2m above current position
 		pos_x = float(r_pos_x)
 		pos_y = float(r_pos_y)
 		pos_z = float(r_pos_z) + float(takeoff_altitude)
+		return "New waypoint (takeoff): x{} y{} z{}".format(pos_x, pos_y, pos_z)
 
-		# return string with takeoff coordinates
-		return_string = "New waypoint (takeoff): x{} y{} z{}".format(pos_x, pos_y, pos_z)
-
-	# drone took off and is in the air
 	if r_payload=='drone_takeoff':
+		# empty memory database
+		server_memory.objects.delete()
 
 		# set state
 		current_state = 0
+		print('STATE: current state is {}'.format(current_state))
+		memory = server_memory(state=current_state)
+		memory.save()
 
-		# first waypoint
+		# send first waypoint
 		pos_x = network_x + circle_radius*math.cos(0)
 		pos_y = network_y + circle_radius*math.sin(0)
 		pos_z = flying_altitude
+		return "New waypoint: x{} y{} z{}".format(pos_x, pos_y, pos_z)
 
-		# return string with new coordinates
-		return_string = "New waypoint: x{} y{} z{}".format(pos_x, pos_y, pos_z)
-
-	# drone reached its previous (unknown) waypoint
 	if r_payload=='waypoint_reached':
-
 		# read old state to increment it
-		current_state = 3#current_state + 1
+		latest = server_memory.objects.order_by('-id').first()
+		current_state = latest.state + 1
+
+		# set state
+		print('STATE: current state is {}'.format(current_state))
+		memory = server_memory(state=current_state, timestamp=r_timestamp, time=r_time)
+		memory.save()
 
 		# send hover time
-		return_string = "Hover time set: h{}".format(hover_time)
+		return "Hover time set: h{}".format(hover_time)
 
-	# drone is hovering in position
 	if r_payload=='data_collected':
+		# read old state to know which waypoint is next
+		latest = server_memory.objects.order_by('-id').first()
 
-		# create and send next waypoint
-		if current_state == 1:
+		# create and send waypoint
+		if latest.state == 1:
 			pos_x = network_x + circle_radius*math.cos(2*math.pi/3)
 			pos_y = network_y + circle_radius*math.sin(2*math.pi/3)
 			pos_z = flying_altitude
-			return_string = "New waypoint: x{} y{} z{}".format(pos_x, pos_y, pos_z)
-		elif current_state == 2:
+		elif latest.state == 2:
 			pos_x = network_x + circle_radius*math.cos(4*math.pi/3)
 			pos_y = network_y + circle_radius*math.sin(4*math.pi/3)
 			pos_z = flying_altitude
-			return_string = "New waypoint: x{} y{} z{}".format(pos_x, pos_y, pos_z)
-		elif current_state == 3:
-			pos_x, pos_y, pos_z = trilateration(drone_dataset)
-			return_string = "Land at position: x{} y{} z{}".format(pos_x, pos_y, pos_z)
 		else:
-			print("ERROR, unknown state")
-
-
-	###################  CREATE MEMORY  ######################
-	# create new datapoint with parsed data
-	datapoint = drone_datapoint()
-	datapoint.pos_x 	= r_pos_x
-	datapoint.pos_y 	= r_pos_y
-	datapoint.pos_z 	= r_pos_z
-	datapoint.time 		= r_time
-	datapoint.timestamp = r_timestamp
-	datapoint.payload   = r_payload
-	datapoint.state 	= current_state
-
-	# save it
-	indice = len(drone_dataset)
-	drone_dataset.append(datapoint)
-
-	trilateration(drone_dataset)
+			pos_x, pos_y, pos_z = trilateration()
+			#pos_x = network_x
+			#pos_y = network_y
+			#pos_z = flying_altitude
+			return "Land at position: x{} y{} z{}".format(pos_x, pos_y, pos_z)
+		return "New waypoint: x{} y{} z{}".format(pos_x, pos_y, pos_z)
 
 	# success
-	return return_string
+	return 'Datapoint added to database'
 
 
 
@@ -439,6 +346,42 @@ def lora_query():
 		headers={'Content-Disposition':'attachment;filename=query.json'})
 
 
+#querying the database and giving back a JSON file
+@app.route('/drone/query', methods=['GET'])
+def drone_query():
+	query = request.args
+
+	# to delete datapoint based on time
+	if 'delete_time_point' in query and 'time' in query:
+		deltime_start = dt.datetime.strptime(query['time'], TIME_FORMAT_QUERY) - dt.timedelta(seconds=2)
+		deltime_end = dt.datetime.strptime(query['time'], TIME_FORMAT_QUERY) + dt.timedelta(seconds=2)
+		drone_datapoint.objects(timestamp__lt=deltime_end, timestamp__gt=deltime_start).delete()
+		return 'point deleted'
+
+	# to delete datapoints based on time
+	if 'delete_time_interval' in query and 'start' in query and 'end' in query:
+		end = dt.datetime.strptime(query['end'], TIME_FORMAT_QUERY)
+		start = dt.datetime.strptime(query['start'], TIME_FORMAT_QUERY)
+		drone_datapoint.objects(timestamp__lt=end,timestamp__gt=start).delete()
+		return 'time interval deleted'
+
+	# defaults: only in the previous year
+	start = dt.datetime.now() - dt.timedelta(days=365)  # begins selection one year ago
+	end = dt.datetime.now() + dt.timedelta(hours=2)		# just in case we have timestamps in the future?
+
+	# change start and end time
+	if 'start' in query:
+		start = dt.datetime.strptime(query['start'], TIME_FORMAT_QUERY)
+	if 'end' in query:
+		end = dt.datetime.strptime(query['end'], TIME_FORMAT_QUERY)
+
+	datapoints = drone_datapoint.objects(timestamp__lt=end,timestamp__gt=start).to_json()
+
+	#return datapoints 		# for directly in browser
+	return Response(datapoints,mimetype='application/json', 	# for automatic file download
+		headers={'Content-Disposition':'attachment;filename=query.json'})
+
+
 
 #########################################################################################
 ####################################  JSON OUTPUT  ######################################
@@ -457,6 +400,28 @@ def lora_export_json():
 def lora_print_json():
 	print('Printing LoRa database as JSON')
 	return LoRa_datapoint.objects.to_json()
+
+
+# output JSON as downloaded file, drone database
+@app.route('/drone/json', methods=['GET'])
+def drone_export_json():
+	print('Exporting drone database as JSON')
+	return Response(drone_datapoint.objects.to_json(),mimetype='application/json',
+		headers={'Content-Disposition':'attachment;filename=database.json'})
+
+
+# print JSON directly in browser, drone database
+@app.route('/drone/json_print', methods=['GET'])
+def drone_print_json():
+	print('Printing drone database as JSON')
+	return drone_datapoint.objects.to_json()
+
+
+# print JSON directly in browser, memory database
+@app.route('/memory/json_print', methods=['GET'])
+def memory_print_json():
+	print('Printing memory database as JSON')
+	return server_memory.objects.to_json()
 
 
 
--- server_app/pylocus-master/test.py
+++ server_app/pylocus-master/test.py
@@ -5,7 +5,23 @@ from pylocus.point_set import PointSet
 from pylocus.algorithms import reconstruct_srls
 from pylocus.simulation import create_noisy_edm, create_mask, create_weights
 from pylocus.basics import mse, rmse
+import math
 
+
+from datetime import datetime
+
+
+
+class drone_datapoints:
+	x = 0
+	y = 0
+
+set_of_data = set()
+set_v2 = []
+
+
+
+### this for testing Pylocus
 points = PointSet(N=5, d=2)
 points.set_points('random')
 print("point to localize:", points.points[0, :])
@@ -24,3 +40,69 @@ error = mse(points_estimated[0, :], points.points[0, :])
 print("estimated point: {}, original point: {}, mse: {:2.2e}".format(points_estimated[0, :],
                                                                 points.points[0, :],
                                                                 error))
+
+
+
+#this for testing set of class
+a = drone_datapoints()
+a.x = 2
+a.y = 5
+a1 = drone_datapoints()
+a1.x = 3
+a1.y = 7
+a2 = drone_datapoints()
+a2.x = 12
+a2.y = 532
+
+print("data is {}".format(a))
+print("data.x is {}".format(a.x))
+
+set_of_data.add(a)
+set_of_data.add(a1)
+set_of_data.add(a2)
+
+print("size is {}".format(len(set_of_data)))
+
+for i in set_of_data:
+	print("i {}, x{}, y{}".format(i, i.x, i.y))
+
+print(set_v2)
+print(type(set_v2))
+
+set_v2.append(a)
+set_v2.append(a1)
+
+print(set_v2)
+
+for i in range(0,len(set_v2)):
+	print(set_v2[i].x)
+
+
+
+
+test = 16
+if test not in {10, 12, 15}:
+	print("yes")
+else:
+	print("no")
+
+
+
+
+timestamp = 1545730073
+datetime_ts = datetime.fromtimestamp(timestamp)
+
+print(datetime_ts)
+print(type(datetime_ts))
+
+adding = 2.5
+timestamp_2 = (datetime_ts- datetime(1970,1,1)).total_seconds()
+print(timestamp_2)
+
+
+
+f_a = 0.2011
+f_b = -0.0556
+esp = -90
+dist = f_a * math.exp(f_b*esp)
+print("dist is", dist)

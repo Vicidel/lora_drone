e6d92619ead6b31d574d0005dadaf24129c69af8
Victor Delafontaine <vicdelafont@gmail.com>
2019-05-13 09:55:05 +0200

    new name for vic_git

--- scripts/copy_vic_package_in_git.sh
+++ scripts/copy_vic_package_in_git.sh
@@ -1,2 +1,2 @@
-rm -rf ~/vic_git/03_drone_implementation/vic_package
-cp -R ~/catkin_ws/src/vic_package ~/vic_git/03_drone_implementation/vic_package
+rm -rf ~/git_vic/03_drone_implementation/vic_package
+cp -R ~/catkin_ws/src/vic_package ~/git_vic/03_drone_implementation/vic_package
